b8ff08c099b34bf2ad02e0252fb830672237b1d0
Mayra Valdes <4valdes@gmail.com>
2020-06-09 21:55:17 -0500

    test: update test for honk

--- __tests__/honk.test.ts
+++ __tests__/honk.test.ts
@@ -2,12 +2,13 @@ import * as github from '@actions/github'
 import * as core from '@actions/core'
 import {WebhookPayload} from '@actions/github/lib/interfaces'
 import nock from 'nock'
-import run from '../honk'
+import run from '../src/honk'
 
 beforeEach(() => {
   // resetModules allows you to safely change the environment and mock imports
   // separately in each of your tests
-  jest.resetModules()
+  // https://github.com/Keyang/node-csvtojson/issues/271
+  // jest.resetModules()
   jest.spyOn(core, 'getInput').mockImplementation((name: string): string => {
     if (name === 'token') return '12345'
     return ''
@@ -46,25 +47,38 @@ describe('honk action', () => {
   })
 
   it('deletes the comment and adds a comment', async () => {
-    // Override the default payload so that it doesn't include "honk". This should make the
-    // action run through more of the code
-    github.context.payload.comment.body = 'No geese allowed'
+    // // Use nock to mock out the external call to create the honk comment
+    // // Setting this up creates an expectation that must be called and returns a 200 response
+    nock('https://api.github.com').get(`/repos/example/repository`).reply(200, {
+      url: 'https://github.com/#example',
+      default_branch: 'master',
+    })
 
-    // Use nock to mock out the external call to delete the comment
-    // Setting this up creates an expectation that must be called and returns a 200 response
-    nock('https://api.github.com').delete('/repos/example/repository/issues/comments/1').reply(200, {})
-
-    // Use nock to mock out the external call to create the honk comment
-    // Setting this up creates an expectation that must be called and returns a 200 response
     nock('https://api.github.com')
-      .post(`/repos/example/repository/issues/1/comments`, (body) => {
-        return (
-          body.body ===
-          '![honk](https://user-images.githubusercontent.com/4064/65900857-cf462f80-e36b-11e9-9a9c-76170c99618b.png)'
-        )
+      .get(`/repos/example/repository/commits?sha=master&per_page=2`)
+      .reply(200, [
+        {
+          sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e',
+        },
+        {
+          sha: '05e95bff4dd8f61038c3450b0064a1c28d55803f',
+        },
+      ])
+
+    nock('https://api.github.com')
+      .patch(`/repos/example/repository/git/refs/refs/heads/master`, (inputs) => {
+        console.log('inputs', inputs.sha == '80d4ead42d33eb92553d5ccc74daa9c620e3e365')
+        return true
       })
-      .reply(200, {
-        url: 'https://github.com/#example',
+      .reply(201, {
+        ref: 'refs/heads/master',
+        node_id: 'MDM6UmVmcmVmcy9oZWFkcy9mZWF0dXJlQQ==',
+        url: 'https://api.github.com/repos/octocat/Hello-World/git/refs/heads/featureA',
+        object: {
+          type: 'commit',
+          sha: 'aa218f56b14c9653891f9e74264a383fa43fefbd',
+          url: 'https://api.github.com/repos/octocat/Hello-World/git/commits/aa218f56b14c9653891f9e74264a383fa43fefbd',
+        },
       })
 
     await run()
